e2ab18f51a5dbb993ea51e98633b673eec7116b0
Muhammadali0202 <muhammadalisajjadakhunzada@gmail.com>
2024-10-19 09:52:34 +0500

    done with factorial using funtions

---
+++ FUNCTION/factorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int fact(int n){
+    int fact= 1;
+    for(int i= 2; i<=n;i++){
+        fact *= i;
+    }
+    return fact;
+}
+int main(){
+    int n;
+    cout << "Enter number to find factorial: ";
+    cin >> n;
+    // for(int i= 1; i<=n;i++){
+    //    cout << fact(i) << endl;   
+    // }
+    for(int i= 2; i<=n;i++){
+        fact *= i;
+        cout << fact << endl;
+    }
+}
