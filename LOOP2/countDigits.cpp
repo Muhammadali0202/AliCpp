07ae571adc3477a9048cac30acc93ff33738e86e
Muhammadali0202 <muhammadalisajjadakhunzada@gmail.com>
2024-10-19 19:41:41 +0500

    correcting question in LOOP2

--- LOOP2/countDigits.cpp
+++ LOOP2/countDigits.cpp
@@ -5,11 +5,14 @@ int main(){
     cout << "Enter a Number: ";
     cin >> num;
     int count = 0;
-    int a = n;
+    int a = num;
     while(num>0){
         num = num/10;
         count++;
     }
-    if a == 0 cout << 1;
-    else cout << count;
+    if( a == 0 ){
+        cout << 1;
+    }else {
+        cout << count;
+    }
 }
