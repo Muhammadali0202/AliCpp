617a77065d53aa6d469f9083bc651e95a7ea8861
Muhammadali0202 <muhammadalisajjadakhunzada@gmail.com>
2024-10-28 15:31:33 +0500

    done with function practice question

---
+++ FUNCTION/funtionAssignment/practiceQuestion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int lastDigit(int n){
+    return n%10;
+}
+int sum(int num){
+    int sumOfDigit = 0;
+    while(num>0){
+        lastDigit(num);
+        sumOfDigit = sumOfDigit + lastDigit(num);
+        num = num/10;
+    }
+     return sumOfDigit;
+
+}
+int main(){
+    int n;
+    cout << "Enter the Number: ";
+    cin >> n;
+
+    cout << sum(n);
+}
