aac5152e0355c82e36c759d4269247a3758acc5a
Muhammadali0202 <muhammadalisajjadkhunzada@gmail.com>
2025-09-02 21:50:40 +0500

    done with making own stack using array

---
+++ STACKS1/arrrayImplementaion.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+class Stack{
+    public:
+    int arr[5];
+    int idx = -1;
+
+    void push(int val){
+        if(idx == 4){
+            cout << "Stack is full" << endl;
+            return;
+        }
+        idx++;
+        arr[idx] = val;
+    }
+
+    void pop(){
+        if(idx == -1){
+            cout << "Stack is Empty"<< endl;
+            return;
+        }
+        idx--;
+    }
+
+    int top(){
+        if(idx == -1){
+            cout << "Stack is Empty"<< endl;
+            return;
+        }
+        return arr[idx];
+    }
+
+    int size(){
+        return idx+1;
+    }
+};
+int main(){
+    Stack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    cout<<st.size() << endl;
+    cout<< st.top() << endl;
+
+}
