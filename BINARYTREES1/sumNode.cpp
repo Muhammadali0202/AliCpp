39fddbbd2c0de82264429cfb4e8616baef08e4cd
Muhammadali0202 <muhammadalisajjadkhunzada@gmail.com>
2025-11-23 13:36:59 +0500

    done with sum of Nodes in a tree

---
+++ BINARYTREES1/sumNode.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+        Node(int val){
+            this->val=val;
+            this->left=NULL;
+            this->right=NULL;
+        }
+};
+
+void displayTree(Node* root){
+    if(root == NULL) return;
+    cout << root->val << " ";
+    displayTree(root->left);
+    displayTree(root->right);
+}
+
+//SUM OF NODES
+int sum(Node* root){
+    if(root == NULL) return 0;
+    return root->val+ sum(root->left)+ sum(root->right);
+}
+
+//SIZE OF TREE
+int size(Node* root){
+    if(root == NULL) return 0;
+    
+    return 1 + size(root->left)+size(root->right);
+}
+
+//MAX OF TREE
+int maxi(Node* root){
+    if(root == NULL) return INT_MIN;
+    int lMax=maxi(root->left);
+    int rMax=maxi(root->right);
+    return max(root->val,max(lMax,rMax));
+}
+
+//LEVEL OF TREE
+int level(Node* root){
+    if(root ==NULL) return 0;
+    return 1+max(level(root->left),level(root->right));
+}
+int main(){
+    Node* a = new Node(1);
+    Node* b = new Node(2);
+    Node* c = new Node(3);
+    Node* d = new Node(4);
+    Node* e = new Node(5);
+    Node* f = new Node(6);
+    Node* g = new Node(7);
+
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+
+    displayTree(a);
+    cout<< endl;
+    cout << sum(a) << endl;
+    cout << size(a) << endl;
+    cout << maxi(a) << endl;
+    cout << level(a) << endl;
+}
